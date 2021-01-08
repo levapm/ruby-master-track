b482947404ee4a8251f80cd90be8f9ace799e18a
@pmanzanares <pmanzanares@greathealthworks.com>
2021-01-08 11:18:02 -0500

    initial commit to show code

---
+++ config/routes.rb
@@ -0,0 +1,59 @@
+Rails.application.routes.draw do
+
+  # You can have the root of your site routed with "root"
+  # root 'welcome#index'
+
+  # Example resource route with options:
+  #   resources :products do
+  #     member do
+  #       get 'short'
+  #       post 'toggle'
+  #     end
+  #
+  #     collection do
+  #       get 'sold'
+  #     end
+  #   end
+
+  # Example resource route with sub-resources:
+  #   resources :products do
+  #     resources :comments, :sales
+  #     resource :seller
+  #   end
+
+  # Example resource route with more complex sub-resources:
+  #   resources :products do
+  #     resources :comments
+  #     resources :sales do
+  #       get 'recent', on: :collection
+  #     end
+  #   end
+
+  # Example resource route with concerns:
+  #   concern :toggleable do
+  #     post 'toggle'
+  #   end
+  #   resources :posts, concerns: :toggleable
+  #   resources :photos, concerns: :toggleable
+
+  # Example resource route within a namespace:
+  #   namespace :admin do
+  #     # Directs /admin/products/* to Admin::ProductsController
+  #     # (app/controllers/admin/products_controller.rb)
+  #     resources :products
+  #   end
+
+
+  # ROOT ROUTE
+  root 'about#index'
+
+  # ROUTES FOR ABOUT PAGE
+  resources :about
+
+  # ROUTES FOR REPORTS
+  resources :report
+
+  # ROUTES FOR SIGHTINGS
+  resources :sightings
+
+end
